ec4ed7f8f005089a15bbb8638fa2e351d2e9da9c
Vikt-art <viktoriya.matvienko86@gmail.com>
2024-02-05 10:48:00 +0100

    test

---
+++ cypress/e2e/homework.cy.js
@@ -0,0 +1,28 @@
+let testData = [{
+  fieldSelector: '#exampleInputEmail1',
+  inputValue: "viktoriya.matvienko86@gmail.com",
+  fieldSelector1: '#exampleInputPassword1',
+  inputValue1: "123456789",
+},
+  {
+    fieldSelector: '#exampleInputEmail1',
+    inputValue: "viktoriya@gmail.com",
+    fieldSelector1: '#exampleInputPassword1',
+    inputValue1: "0987654321",
+
+  }]
+
+testData.forEach(testData => {
+  it(`Field ${testData.fieldSelector} is required: ${testData}`, () => {
+    cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com/');
+    cy.get('[alt="Material Dark Theme"]').click();
+    cy.get('a[title="Forms"]').click();
+    cy.get('[title="Form Layouts"]').click();
+    cy.get(testData.fieldSelector).type(`${testData.inputValue}`);
+    cy.get(testData.fieldSelector1).type(`${testData.inputValue1}`);
+    cy.get('nb-card:contains("Basic form") .custom-checkbox').click();
+    cy.get('nb-card:contains("Basic form") .appearance-filled size-medium shape-rectangle status-danger nb-transition').click();
+  })
+})
+
+
